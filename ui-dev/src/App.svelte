ea4f6506140acf79071430a4986010ca08891033
victor-twu <victor.twu@slalom.com>
2022-05-06 22:22:22 -0700

    minimal boiler plate

--- ui-dev/src/App.svelte
+++ ui-dev/src/App.svelte
@@ -1,15 +1,37 @@
 <script>
-	export let name;
+	let firstName = 'Victor'
+	let lastName = 'Twu'
+	let yellow = false
+	
+	//reactive value
+	
+	$: color = yellow ? 'yellow' : '#ff3e00'
+	$: name = firstName + ' ' + lastName
+	
+	const changeName = () => {
+		if (lastName === 'Twu') {
+			lastName = '607'
+		} else{
+			lastName = 'Twu'
+		}
+	}
+
+	const changeColor = () => {
+		yellow = !yellow
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 style='color: {color}'>Hello {name}!</h1>
+	<button on:click={changeName}>toggle name last name</button>
+	<button on:click={changeColor}>toggle color</button>
 </main>
 
 <style>
 	main {
+		height: 100vh;
 		text-align: center;
+		background-color: #272727;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
