d04c72859ed78556b0d41fa15f02f41c1d1d5e54
victor-twu <victor.twu@slalom.com>
2022-05-21 11:54:42 -0700

    positioned the card contents

--- ui-dev/src/components/CartItem.svelte
+++ ui-dev/src/components/CartItem.svelte
@@ -5,17 +5,42 @@
     export let removeProduct
 </script>
 
-<div>
-    <h3>{cartItem.make} {cartItem.model} QTY: {cartItem.qty}</h3>
-    <button on:click={()=> {
-        addItem(cartItem.id)
-        
-        }}>+</button>
-    <button on:click={()=> {
-        decrementItem(cartItem.id)
+<div class='card'>
+    <button on:click={()=> removeProduct(cartItem.id)}>X</button>
+    <h3>{cartItem.year} {cartItem.make} {cartItem.model} </h3>
+    <div class="addBtnsContainer">
+        <div class="qty">
+            <span>Qty:</span>
+            <span>{cartItem.qty}</span>
+        </div> 
+        <div class='addBtns'>
+
+            <button on:click={()=> addItem(cartItem.id)}>+</button>
+            <button on:click={()=> decrementItem(cartItem.id)}>-</button>
+        </div>
+        <div class="subTotalContainer">
+            <span>Sub Total:</span>
+            <span>{cartItem.subTotal}</span>
+        </div>
+    </div>
+
+   
   
-        }}>-</button>
-    <button on:click={()=> removeProduct(cartItem.id)}>Remove Product</button>
-    sub total: {cartItem.subTotal}
     
 </div>
+
+<style>
+    .card{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+    .addBtnsContainer {
+        display: flex;
+        align-items: center;
+    }
+    .addBtns, .qty, .subTotalContainer {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
