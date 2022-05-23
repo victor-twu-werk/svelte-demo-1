37d1dc93893439117b8925e1b71fab54aecc747e
victor-twu <victor.twu@slalom.com>
2022-05-23 12:16:15 -0700

    styled items in cart

--- ui-dev/src/components/CartItem.svelte
+++ ui-dev/src/components/CartItem.svelte
@@ -6,17 +6,17 @@
 </script>
 
 <div class='card'>
-    <button on:click={()=> removeProduct(cartItem.id)}>X</button>
+    <button class="removeBtn" on:click={()=> removeProduct(cartItem.id)}>&#10005;</button>
     <h3>{cartItem.year} {cartItem.make} {cartItem.model} </h3>
     <div class="addBtnsContainer">
         <div class="qty">
             <span>Qty:</span>
-            <span>{cartItem.qty}</span>
+            <span class="qtyValue">{cartItem.qty}</span>
         </div> 
         <div class='addBtns'>
 
-            <button on:click={()=> addItem(cartItem.id)}>+</button>
-            <button on:click={()=> decrementItem(cartItem.id)}>-</button>
+            <button class="incrementBtn" on:click={()=> addItem(cartItem.id)}>&#10095;</button>
+            <button class="decrementBtn" on:click={()=> decrementItem(cartItem.id)}>&#10094;</button>
         </div>
         <div class="subTotalContainer">
             <span>Sub Total:</span>
@@ -34,6 +34,22 @@
         width: 100%;
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        margin: 1rem 0 1rem;
+        padding: .5rem;
+        box-shadow: var(--lightShadow);
+        color: var(--greenAqua)
+    }
+    .removeBtn, .incrementBtn, .decrementBtn {
+        border: none;
+        background: none;
+        color: var(--saffron);
+        transition: .3s;
+    }
+    .removeBtn:hover {
+        cursor: pointer;
+        transform: scale(1.2);
+        color: red;
     }
     .addBtnsContainer {
         display: flex;
@@ -42,5 +58,30 @@
     .addBtns, .qty, .subTotalContainer {
         display: flex;
         flex-direction: column;
+        margin: .5rem;
+    }
+    .qtyValue {
+        font-weight: bolder;
+    }
+    .incrementBtn {
+        transform: rotate(270deg);
+    }
+    .decrementBtn {
+        transform: rotate(-90deg);
+    }
+    .incrementBtn:hover {
+        cursor: pointer;
+        transform: rotate(270deg) scale(1.2);
+        color: green;
+    }
+    .decrementBtn:hover {
+        cursor: pointer;
+        transform: rotate(-90deg) scale(1.2);
+        color: red;
+    }
+    .subTotalContainer {
+        min-width: 8rem;
     }
 </style>
+    
+   
